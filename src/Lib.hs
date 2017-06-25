7a7e9ab54920d85119555cc0e2a2f799a63c542a
andy <andy@ashall.co>
2017-06-25 17:06:12 +0100

    started on haskell - unit done

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,10 +5,21 @@ module Lib
 run :: IO ()
 run = do
   let d = square 4
-  print d
+  let p = cubeDebug (unit 4)
+  print p
+
+-- Unit
+
+unit :: Float -> (Float, [Char])
+unit v = (v, "")
+
+-- Funcs
 
 square :: Float -> (Float, [Char])
 square n = (n * n, "Square was called")
 
 cube :: Float -> (Float, [Char])
 cube n = (n * n * n, "Cube was called")
+
+cubeDebug :: (Float, [Char]) -> (Float, [Char])
+cubeDebug (n, s) = (n * n * n, s ++ " " ++ "Cube was called")
