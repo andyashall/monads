b2daa1be96428b8ea1a7d2d4c298471db2c8ec7e
andyashall <andy@ashall.co>
2017-06-27 16:07:45 +0100

    added bind

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,15 +4,18 @@ module Lib
 
 run :: IO ()
 run = do
-  let d = square 4
-  let p = cubeDebug (unit 4)
-  let n = cubeDebug (square 4)
-  print n
+  let p = bind square (bind cube (unit 4))
+  print p
 
 -- Unit
 
 unit :: Float -> (Float, [Char])
 unit v = (v, "")
+
+-- Bind
+
+bind :: (Float -> (Float, [Char])) -> ((Float, [Char]) -> (Float, [Char]))
+bind fun (a1,a2) = let (b1,b2) = fun a1 in (b1,a2++b2)
 
 -- Funcs
 
@@ -21,6 +24,3 @@ square n = (n * n, "Square was called")
 
 cube :: Float -> (Float, [Char])
 cube n = (n * n * n, "Cube was called")
-
-cubeDebug :: (Float, [Char]) -> (Float, [Char])
-cubeDebug (n, s) = (n * n * n, s ++ " " ++ "Cube was called")
